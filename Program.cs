46af433e2c81ae9fbdc186a5c52b86fec0b82440
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-02-24 18:42:30 +0000

    created program base

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.XPath;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(GetFizzBuzz());
+        }
+        public static string GetFizzBuzz()
+        {
+            return "result";
+        }
+
+    }
+}
